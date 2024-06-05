1064aa4dcb048f84d2b5fbf6643cca47f88fd3aa
xuyan <17862925461@163.com>
2024-06-05 13:32:20 +0200

    sb3 params tuning

--- train_sb3.py
+++ train_sb3.py
@@ -5,10 +5,13 @@
     pipeline with an RL algorithm of your choice between PPO and SAC.
 """
 import gym
+import optuna
+
 from env.custom_hopper import *
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
+
 
 def train_model(env_id):
     train_env = gym.make(env_id)
@@ -52,6 +55,39 @@ def test():
         if dones:
             obs = env.reset()
 
+def tuning(trial):
+    n_steps = trial.suggest_int('n_steps', 2048, 8192)
+    gamma = trial.suggest_float('gamma', 0.9, 0.9999, log=True)
+    learning_rate = trial.suggest_float('learning_rate', 1e-5, 1e-2, log=True)
+    ent_coef = trial.suggest_float('ent_coef', 0.0, 0.1)
+    clip_range = trial.suggest_float('clip_range', 0.1, 0.4)
+    n_epochs = trial.suggest_int('n_epochs', 1, 10)
+    
+    env = make_vec_env(lambda: gym.make('CustomHopper-source-v0'), n_envs=4)
+
+    model = PPO(
+        "MlpPolicy",
+        env,
+        n_steps=n_steps,
+        gamma=gamma,
+        learning_rate=learning_rate,
+        ent_coef=ent_coef,
+        clip_range=clip_range,
+        n_epochs=n_epochs,
+        verbose=0 # close log output
+    )
+    model.learn(total_timesteps=100000)
+    mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=10)
+
+    return mean_reward
+
+def get_best_params():
+    study = optuna.create_study(direction='maximize')
+    study.optimize(tuning, n_trials=100)
+    print("Best params: ", study.best_params)
+
+
 if __name__ == '__main__':
     #main()
-    test()
+    #test()
+    get_best_params()
